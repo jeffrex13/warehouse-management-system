cfcb66a485b4ee539552e79ee28bc2a04ab0f638
Jeffrey Cureg <jepcureg@gmail.com>
2021-05-02 16:26:46 +0800

    (WMS-19) Created search UI for admin and made changes to the links in the sidebar for inventory

--- Admin-Search/admin-search.php
+++ Admin-Search/admin-search.php
@@ -13,12 +13,11 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/admin-inventory.css">
+    <link rel="stylesheet" href="../CSS/admin-search.css">
     <link rel="icon" href="https://static.thenounproject.com/png/165116-200.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <!--Dropdown-->
@@ -26,7 +25,7 @@
     <!-- Search Icon -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
         integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <title>Admin Inventory</title>
+    <title>Search</title>
 </head>
 
 <body id="body">
@@ -36,9 +35,9 @@
         <a href="../Product-registration/product-registration.php">Product Registration</a>
         <a href="#">Profile</a>
         <a href="../Admin-Gallery/admin-gallery.php">Gallery</a>
-        <a href="./admin-inventory.php" style="color: #4B778D">Inventory</a>
+        <a href="../Admin-Inventory/admin-inventory.php">Inventory</a>
         <a href="#">Sales and Purchase</a>
-        <a href="../Admin-Search/admin-search.php">Search</a>
+        <a href="./admin-search.php" style="color: #4B778D">Search</a>
         <button class="dropdown-btn">Product Monitoring
             <i class="fa fa-caret-down"></i>
         </button>
@@ -66,17 +65,17 @@
             <a href="#">Restore</a>
         </div>
         <a href="#">Help</a>
-        <a href="admin-inventory.php?logout=<?php echo " $username"?>">Logout</a>
+        <a href="admin-search.php?logout=<?php echo " $username"?>">Logout</a>
     </div>
     <div id="main">
         <button class="openbtn" onclick="openNav()">&#9776; Open Menu</button>
         <div class="container">
-            <h1 class="inv-header">Inventory</h1>
-            <form action="" method="post">
+            <h1 class="search-header">Search</h1>
+            <form action="" method="post" class="search-form">
                 <label for="search">Search</label>
-                <input type="text" name="search" id="search" placeholder="Search for product">
-                <label for="product-category">Search by</label>
-                <select id="product-category" name="product">
+                <input type="text" name="search" id="search" placeholder="Search">
+                <!-- <label for="search-category">Search by</label>
+                <select id="search-category" name="search">
                     <option value="product-id">Product ID</option>
                     <option value="brand-name">Brand Name</option>
                     <option value="type">Type</option>
@@ -84,18 +83,18 @@
                     <option value="color">Color</option>
                     <option value="quantity">Quantity</option>
                     <option value="price">Price</option>
-                </select>
+                </select> -->
                 <input type="submit" value="Search">
             </form>
             <table>
                 <thead>
-                    <th>Product ID</th>
-                    <th>Brand Name</th>
-                    <th>Type</th>
-                    <th>Model</th>
-                    <th>Color</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
+                    <th>Sample</th>
+                    <th>Sample</th>
+                    <th>Sample</th>
+                    <th>Sample</th>
+                    <th>Sample</th>
+                    <th>Sample</th>
+                    <th>Sample</th>
                 </thead>
                 <tbody>
                     <tr>
@@ -124,6 +123,15 @@
                         <td>Black</td>
                         <td>3</td>
                         <td>Php 4,698.00</td>
+                    </tr>
+                    <tr>
+                        <td>Sample</td>
+                        <td>Sample</td>
+                        <td>Sample</td>
+                        <td>Sample</td>
+                        <td>Sample</td>
+                        <td>Sample</td>
+                        <td>Sample</td>
                     </tr>
                 </tbody>
             </table>
