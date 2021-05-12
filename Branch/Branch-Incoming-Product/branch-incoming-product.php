53cd2c5643b13e44caec016f5cb538685d9124d0
Jeffrey Cureg <jepcureg@gmail.com>
2021-05-12 22:58:07 +0800

    created incoming product ui for branch

---
+++ Branch/Branch-Incoming-Product/branch-incoming-product.php
@@ -0,0 +1,92 @@
+?php
+    session_start();
+    $db = mysqli_connect('localhost', 'root', '', 'warehouse_management_system');
+
+    $username = $_SESSION['username'];
+    if (!isset($_SESSION['username'])) {
+        header('location: ../index.php');
+    }
+    if (isset($_GET['logout'])) {
+        date_default_timezone_set('Asia/Manila');
+        $time = date("h:i a");
+        $date = date("D M j, Y");
+
+        $query = "UPDATE tbl_audit_trail SET timeout = '$time', date = '$date' 
+        WHERE username='$username' AND timeout IS NULL";
+        mysqli_query($db, $query);
+        session_destroy();
+        unset($_SESSION['username']);
+        header("location: ../index.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/branch-incoming-product.css">
+    <link rel="icon" href="https://static.thenounproject.com/png/165116-200.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Branch Incoming Product</title>
+</head>
+
+<body>
+    <div id="mySidebar" class="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="../Branch-Homepage/branch-homepage.php">Profile</a>
+        <a href="#">Gallery</a>
+        <a href="#">Search</a>
+        <a href="#">Inventory</a>
+        <a href="./branch-incoming-product.php" style="color: #4B778D">Incoming Product</a>
+        <a href="#">Billing</a>
+        <a href="#">Help</a>
+        <a href="branch-incoming-product.php?logout=<?php echo " $username"?>">Logout</a>
+    </div>
+    <div id="main">
+        <button class="openbtn" onclick="openNav()">&#9776; Open Menu</button>
+        <div class="container">
+                <h1 class="incoming-product-h1">Incoming Product</h1>
+            <table>
+                <thead>
+                    <th>Date</th>
+                    <th>Brand Name</th>
+                    <th>Type</th>
+                    <th>Model</th>
+                    <th>Quantity</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Jan. 6, 2021</td>
+                        <td>Hanabishi</td>
+                        <td>Air-Condition</td>
+                        <td>HTAC25S</td>
+                        <td>15</td>
+                        <td><button class="received-btn">Received</button></td>
+                    </tr>
+                    <tr>
+                        <td>Jan. 11, 2021</td>
+                        <td>Hanabishi</td>
+                        <td>Air Circulator Fan</td>
+                        <td>HACF88</td>
+                        <td>30</td>
+                        <td><button class="received-btn">Received</button></td>
+                    </tr>
+                    <tr>
+                        <td>Feb. 1, 2021</td>
+                        <td>Whirlpool</td>
+                        <td>Microwave Oven</td>
+                        <td>MWX203BL</td>
+                        <td>3</td>
+                        <td><button class="received-btn">Received</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="../index.js"></script>
+</body>
+</html>
