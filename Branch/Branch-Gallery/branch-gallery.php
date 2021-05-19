fdb50a049493a1610c9e4eb1eba0245979c69953
Jeffrey Cureg <jepcureg@gmail.com>
2021-05-19 09:42:32 +0800

    Created Gallery UI for Branch

---
+++ Branch/Branch-Gallery/branch-gallery.php
@@ -0,0 +1,70 @@
+<?php
+    session_start();
+    $db = mysqli_connect('localhost', 'root', '', 'warehouse_management_system');
+
+    $username = $_SESSION['username'];
+    if (!isset($_SESSION['username'])) {
+        header('location: ../index.php');
+    }
+    if (isset($_GET['logout'])) {
+        date_default_timezone_set('Asia/Manila');
+        $time = date("h:i a");
+        $date = date("D M j, Y");
+
+        $query = "UPDATE tbl_audit_trail SET timeout = '$time', date = '$date' 
+        WHERE username='$username' AND timeout IS NULL";
+        mysqli_query($db, $query);
+        session_destroy();
+        unset($_SESSION['username']);
+        header("location: ../index.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/branch-gallery.css">
+    <link rel="icon" href="https://static.thenounproject.com/png/165116-200.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Branch Gallery</title>
+</head>
+
+<body>
+    <div id="mySidebar" class="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="../Branch-Homepage/branch-homepage.php">Profile</a>
+        <a href="./branch-gallery.php" style="color: #4B778D">Gallery</a>
+        <a href="#">Search</a>
+        <a href="../Branch-Inventory/branch-inventory.php">Inventory</a>
+        <a href="../Branch-Incoming-Product/branch-incoming-product.php">Incoming Product</a>
+        <a href="#">Billing</a>
+        <a href="#">Help</a>
+        <a href="branch-gallery.php?logout=<?php echo " $username"?>">Logout</a>
+    </div>
+    <div id="main">
+        <button class="openbtn" onclick="openNav()">&#9776; Open Menu</button>
+        <div class="container">
+            <h1 class="BG-header">Gallery</h1>
+            <div class="slideshow-container fade">
+
+                                <div class="Containers">
+                                    <div class="MessageInfo"></div>
+                                    <img src="../Photos/blender.jpeg" style="width:100%">
+                                </div>
+                                <div class="Containers">
+                                    <div class="MessageInfo"></div>
+                                    <img src="../Photos/stove.jpeg" style="width:100%">
+                                </div>
+                <a class="Back" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="forward" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+        </div>
+    </div>
+    <script src="./index1.js"></script>
+</body>
+
+</html>
